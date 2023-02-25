1155c38af8fd5bfdfde83bae82d836cd02b8a671
CoolDude034 <67103440+CoolDude034@users.noreply.github.com>
2023-02-25 20:44:48 +0100

    Add files via upload

--- GangDispatch/GangDispatch/Main.cs
+++ GangDispatch/GangDispatch/Main.cs
@@ -16,8 +16,11 @@ namespace GangDispatch
         Ped sniper;
         bool isSniperSpawned = false;
 
+        float timer;
+
         WeaponHash[] UNIT_WEAPONS = { WeaponHash.SMG, WeaponHash.CarbineRifle, WeaponHash.PumpShotgun };
         int MAX_UNITS;
+        int MAX_WANTED_LEVEL;
         float MIN_POLICE_SPAWN_DISTANCE;
         float MIN_DISTANCE_FROM_SNIPER_SPAWNS;
 
@@ -46,6 +49,22 @@ namespace GangDispatch
             new Vector3(923.5157f, -2399.00366f, 40.1732559f),
             new Vector3(839.927063f, -2305.346f, 50.8185f),
             new Vector3(804.83f, -2311.378f, 54.57426f),
+            // Downtown LS
+            new Vector3(-270.925323f, -585.946838f, 51.01512f),
+            new Vector3(-270.925323f, -599.1961f, 51.01512f),
+            new Vector3(-324.855164f, -619.5216f, 58.4729729f),
+            new Vector3(-575.951965f, -1049.85181f, 32.37606f),
+            new Vector3(-584.3051f, -1029.52808f, 32.3760948f),
+            // ResDistr Rooftop Snipers
+            new Vector3(-999.821167f, -1207.19641f, 14.3100691f),
+            new Vector3(-1089.90051f, -1229.69678f, 13.4221287f),
+            // Port Area near LSIA
+            new Vector3(-440.312134f, -2816.76172f, 16.4541035f),
+            new Vector3(-430.2499f, -2825.95044f, 16.4541626f),
+            new Vector3(-330.218872f, -2779.36426f, 12.6000147f),
+            // Mission Row
+            new Vector3(352.3809f, -967.0717f, 34.6470642f),
+            new Vector3(360.449371f, -967.0417f, 34.6899643f),
         };
 
         Ped SpawnUnit(Model model, Vector3 pos, Model wpn)
@@ -85,10 +104,12 @@ namespace GangDispatch
         {
             Config = ScriptSettings.Load("scripts/" + this.Filename + ".ini");
             MAX_UNITS = Settings.GetValue<int>("SETTINGS", "MAX_UNITS", 8);
+            MAX_WANTED_LEVEL = Settings.GetValue<int>("SETTINGS", "MAX_WANTED_LEVEL", 3);
             MIN_POLICE_SPAWN_DISTANCE = Settings.GetValue<float>("SETTINGS", "MIN_POLICE_SPAWN_DISTANCE", 100f);
             MIN_DISTANCE_FROM_SNIPER_SPAWNS = Settings.GetValue<float>("SETTINGS", "MIN_DISTANCE_FROM_SNIPER_SPAWNS", 500f);
 
             Tick += OnTick;
+            Aborted += ScriptCleanup;
         }
 
         bool canSpawn()
@@ -139,35 +160,24 @@ namespace GangDispatch
         PedHash GetModelByZone()
         {
             Vector3 myPos = Game.Player.Character.Position;
-            Vector2 pos = new Vector2(myPos.X, myPos.Y);
+            //Vector2 pos = new Vector2(myPos.X, myPos.Y);
+            char zoneId = Function.Call<char>(Hash.GET_NAME_OF_ZONE, myPos.X, myPos.Y, myPos.Z);
+            var current_zone = zoneId.ToString();
 
-            var current_zone = World.GetStreetName(pos);
-
-            if (current_zone == "ARMYB" || current_zone == "ZANCUDO")
+            if (current_zone == "Army" || current_zone == "Army1")
             {
                 return PedHash.Marine03SMY;
             }
-            else if (current_zone == "NOOSE" || current_zone == "HUMLAB")
-            {
-                return PedHash.Swat01SMY;
-            }
-            else if (current_zone == "ELYSIAN")
+            else if (current_zone == "ZElys" || current_zone == "ZElys1")
             {
                 return PedHash.Blackops02SMY;
             }
-            else if (current_zone == "PROL")
+            else if (current_zone == "PrLog")
             {
                 return PedHash.Snowcop01SMM;
             }
-            else if (current_zone == "ISHeist")
-            {
-                return PedHash.CartelGuards01GMM;
-            }
-            else
-            {
-                return PedHash.Swat01SMY;
-            }
 
+            return PedHash.Swat01SMY;
         }
 
         void UpdateState()
@@ -185,7 +195,7 @@ namespace GangDispatch
 
         void UpdateGroups()
         {
-            if (Game.Player.WantedLevel >= 3 && canSpawn())
+            if (Game.Player.WantedLevel >= MAX_WANTED_LEVEL && canSpawn())
             {
                 bool IS_SEEN_BY_COPS = Function.Call<bool>(Hash.IS_WANTED_AND_HAS_BEEN_SEEN_BY_COPS, Game.Player);
                 if (Game.Player.Character.IsOnFoot && IS_SEEN_BY_COPS)
@@ -247,6 +257,34 @@ namespace GangDispatch
             UpdateState();
             UpdateGroups();
             UpdateExistingMembers();
+        }
+
+        void ScriptCleanup(object sender, EventArgs e)
+        {
+            Tick -= OnTick;
+
+            if (sniper != null && sniper.Exists())
+            {
+                sniper.MarkAsNoLongerNeeded();
+                sniper = null;
+                isSniperSpawned = false;
+            }
+
+            if (groups.Count > 0)
+            {
+                for (int i = groups.Count - 1; i > -1; i--)
+                {
+                    var ped = groups[i];
+                    if (ped != null && ped.Exists())
+                    {
+                        ped.MarkAsNoLongerNeeded();
+                        groups.RemoveAt(i);
+                    }
+                }
+
+            }
+
+            ToggleDispatchServices(true);
         }
 
         void ToggleDispatchServices(bool toggle)
